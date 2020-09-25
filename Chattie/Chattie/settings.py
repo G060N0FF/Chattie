4a3d7d0545d8bdaa672632c6aa935e53496b014c
G060N0FF <georgi.hr.dim@gmail.com>
2020-09-25 18:39:35 +0300

    Added installed apps to settings

--- Chattie/Chattie/settings.py
+++ Chattie/Chattie/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'Registration',
+    'Direct_messages',
+    'Group_messages',
+    'Chatbot_messages',
 ]
 
 MIDDLEWARE = [

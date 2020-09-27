28e171855ec714f6dc6bc19c6d4ace233aedd9ab
peshoFTW <vikikoy@abv.bg>
2020-09-27 11:53:57 +0300

    Registration

--- Chattie/Chattie/settings.py
+++ Chattie/Chattie/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Registration',
+    'Home',
     'Direct_messages',
     'Group_messages',
     'Chatbot_messages',
--- Chattie/Chattie/urls.py
+++ Chattie/Chattie/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('django.config.auth')),
+    path('chat_bot/', include('Chatbot_messages.urls')),
+    path('direct_messages/', include('Direct_messages.urls')),
+    path('group_messages/', include('Group_messages.urls')),
+    path('/', include('Home.urls')),
 ]

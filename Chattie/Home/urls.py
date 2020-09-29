5e6c3f703453240619c7c5468027a48d8c841fd6
G060N0FF <georgi.hr.dim@gmail.com>
2020-09-29 20:52:37 +0300

    Home page

--- Chattie/Home/urls.py
+++ Chattie/Home/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('accounts/', include('django.contrib.auth.urls')),
     path('sign_up/', views.sign_up, name='sign_up'),
 ]
--- Chattie/Home/views.py
+++ Chattie/Home/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login
 
 
 def sign_up(request):
@@ -10,7 +12,14 @@ def sign_up(request):
         if form.is_valid():
             user = form.save()
             user.save()
+            login(request, user)
             return redirect('/')
     context = {"form": form}
 
     return render(request, 'registration/sign_up.html', context)
+
+
+@login_required
+def index(request):
+    context = {}
+    return render(request, 'Home/index.html', context)

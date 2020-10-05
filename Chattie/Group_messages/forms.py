70e0f4d34f3346ae9702212ea44c2d99a7c78459
G060N0FF <georgi.hr.dim@gmail.com>
2020-10-05 23:35:49 +0300

    Group creation

--- Chattie/Group_messages/forms.py
+++ Chattie/Group_messages/forms.py
@@ -2,4 +2,8 @@ from django import forms
 
 
 class GroupCodeForm(forms.Form):
-    code = forms.CharField(max_length=200)
+    code = forms.CharField(max_length=10)
+
+
+class GroupCreationForm(forms.Form):
+    name = forms.CharField(max_length=200)
--- Chattie/Group_messages/views.py
+++ Chattie/Group_messages/views.py
@@ -1,26 +1,53 @@
 from django.shortcuts import render, redirect
-from .forms import GroupCodeForm
+from .forms import GroupCodeForm, GroupCreationForm
 from .models import Group
 from django.contrib.auth.decorators import login_required
+
+import random
+import string
+
+
+def get_random_alphanumeric_string():
+    letters_and_digits = string.ascii_letters + string.digits
+    result_str = ''.join((random.choice(letters_and_digits) for _ in range(10)))
+    return result_str
 
 
 @login_required
 def select_group(request):
-    form = GroupCodeForm()
+    join_form = GroupCodeForm()
+    creation_form = GroupCreationForm()
     groups = request.user.user_groups.all()
 
     if request.method == "POST":
-        form = GroupCodeForm(request.POST)
-        if form.is_valid():
-            code = request.POST['code']
-            try:
-                group = Group.objects.get(code=code)
-            except:
-                context = {'error': "There is not a group that matches this password"}
-                return render(request, 'Group_messages/error.html', context)
-            return redirect('/chat/'+str(group.pk))
+        if 'code' in request.POST:
+            form = GroupCodeForm(request.POST)
+            if form.is_valid():
+                code = request.POST['code']
+                try:
+                    group = Group.objects.get(code=code)
+                except:
+                    context = {'error': "There is not a group that matches this password"}
+                    return render(request, 'Group_messages/error.html', context)
+                return redirect('/chat/'+str(group.pk))
+        elif 'name' in request.POST:
+            form = GroupCreationForm(request.POST)
+            if form.is_valid():
+                name = request.POST['name']
+                while True:
+                    try:
+                        new_group = Group(name=name, code=get_random_alphanumeric_string())
+                        new_group.save()
+                        new_group.users.add(request.user)
+                        new_group.save()
+                        break
+                    except:
+                        print("Trying new code")
+                return redirect('/group_messages')
+
+
                 
-    context = {'form': form, 'groups': groups}
+    context = {'joinForm': join_form, 'creationForm': creation_form, 'groups': groups}
     return render(request, 'Group_messages/select_group.html', context)
 
 

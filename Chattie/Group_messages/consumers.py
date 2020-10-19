a538f4acb27efac848bf517440480b71f1f334c8
G060N0FF <georgi.hr.dim@gmail.com>
2020-10-19 14:59:24 +0300

    Save message into database

--- Chattie/Group_messages/consumers.py
+++ Chattie/Group_messages/consumers.py
@@ -2,6 +2,10 @@ import json
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
 
+from .models import Group, GroupMessage
+from django.contrib.auth.models import User
+
+
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room_name']
@@ -24,19 +28,26 @@ class ChatConsumer(WebsocketConsumer):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         username = text_data_json['username']
+        room_id = text_data_json['roomName']
 
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
-                'username': username
+                'username': username,
+                'room_id': room_id
             }
         )
 
     def chat_message(self, event):
         message = event['message']
         username = event['username']
+        room_id = event['room_id']
+        user = User.objects.get(username=username)
+        group = Group.objects.get(pk=room_id)
+        new_message = GroupMessage(group=group, user=user)
+        new_message.save()
 
         self.send(text_data=json.dumps({
             'message': message,
